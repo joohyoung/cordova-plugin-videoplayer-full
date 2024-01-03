1d321551945436db758d85f7c90b6624c88665b7
jhpark <akalpa@naver.com>
2024-01-03 11:54:56 +0900

    영상 재생이 끝났을 때 자동으로 닫히도록 구현.

--- src/ios/VideoPlayer.m
+++ src/ios/VideoPlayer.m
@@ -34,6 +34,11 @@
     if(@available(iOS 11.0, *)) {
         [playerViewController setEntersFullScreenWhenPlaybackBegins:YES];
     }
+    // 영상 재생이 끝났을 때 알림을 받을 수 있도록 옵저버 추가
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerDidFinishPlaying:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:player.currentItem];
 
     [self.viewController presentViewController:playerViewController animated:NO completion:^(void){
         [player play];
@@ -51,6 +56,16 @@
 
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+// 영상 재생이 끝난 후에 호출될 메서드
+- (void)playerDidFinishPlaying:(NSNotification *)notification
+{
+    // 옵저버 제거
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+
+    // 영상 재생이 완료되면 playerViewController를 닫음
+    [playerViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
